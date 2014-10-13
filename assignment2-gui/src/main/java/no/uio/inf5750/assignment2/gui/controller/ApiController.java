76d3ea7fd45a693cec643ab5838addd66a312641
hckiella <hckiella@gmail.com>
2014-10-14 01:01:57 +0200

    Further ApiController implementation

--- assignment2-gui/src/main/java/no/uio/inf5750/assignment2/gui/controller/ApiController.java
+++ assignment2-gui/src/main/java/no/uio/inf5750/assignment2/gui/controller/ApiController.java
@@ -3,7 +3,6 @@ package no.uio.inf5750.assignment2.gui.controller;
 import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import no.uio.inf5750.assignment2.model.Course;
 import no.uio.inf5750.assignment2.model.Degree;
@@ -35,8 +34,11 @@ public class ApiController {
 	
 	@RequestMapping(value = "/api/student/{student}/location", method = RequestMethod.GET)
 	@ResponseBody
-	public Collection<Student> setStudentLocation(@PathVariable String student, HttpServletRequest request,	HttpServletResponse response) {
-
+	public Collection<Student> setStudentLocation(@PathVariable String studentId, HttpServletRequest request) {
+		Student student = studentSystem.getStudent(Integer.parseInt(studentId));
+		student.setLatitude(request.getParameter("latitude"));
+		student.setLongitude(request.getParameter("longitude"));
+		
 		Collection<Student> students = studentSystem.getAllStudents();
 		return students;
 	}
